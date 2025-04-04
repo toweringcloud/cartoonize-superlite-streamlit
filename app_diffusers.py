4d1240f5961b04c749b5d4ea7c63d687b83ac955
toweringcloud <toweringcloud@gmail.com>
2025-04-05 02:33:23 +0900

    replicate generative ai

--- app_diffusers.py
+++ app_diffusers.py
@@ -1,10 +1,7 @@
 from diffusers import StableDiffusionControlNetPipeline, ControlNetModel
 from dotenv import dotenv_values
-from io import BytesIO
 from PIL import Image
-from requests_toolbelt.multipart.encoder import MultipartEncoder
 import openai
-import requests
 import streamlit as st
 import torch
 
@@ -20,18 +17,6 @@ st.title("Cartoonize your Photo")
 with st.sidebar:
     IS_TEST = True
     config = dotenv_values(".env")
-
-    # Storage API Credential
-    CLOUDFLARE_ACCOUNT_ID = (
-        st.text_input("Input your Cloudflare Account ID", type="password")
-        if IS_TEST == True
-        else config["CLOUDFLARE_ACCOUNT_ID"]
-    )
-    CLOUDFLARE_API_TOKEN = (
-        st.text_input("Input your Cloudflare API Token", type="password")
-        if IS_TEST == True
-        else config["CLOUDFLARE_API_TOKEN"]
-    )
 
     # LLM API Credential
     OPENAI_API_KEY = (
@@ -57,47 +42,13 @@ with st.sidebar:
     # Link to Github Repo
     st.markdown("---")
     github_link = (
-        "https://github.com/toweringcloud/cartoonize-gpt/blob/main/app_openapi.py"
+        "https://github.com/toweringcloud/cartoonize-gpt/blob/main/app_diffusers.py"
     )
     badge_link = "https://badgen.net/badge/icon/GitHub?icon=github&label"
     st.write(f"[![Repo]({badge_link})]({github_link})")
 
 
-def upload_image_to_cloudflare(image_file):
-    CLOUDFLARE_VERIFY_URL = f"https://api.cloudflare.com/client/v4/accounts/{CLOUDFLARE_ACCOUNT_ID}/tokens/verify"
-    response = requests.get(
-        CLOUDFLARE_VERIFY_URL,
-        headers={"Authorization": f"Bearer {CLOUDFLARE_API_TOKEN}"},
-    )
-    print(f"[verify] {response} | {response.text}")
-
-    if response.status_code != 200:
-        st.error(f"Failed to verify: {response.text}")
-        return None
-
-    CLOUDFLARE_UPLOAD_URL = f"https://api.cloudflare.com/client/v4/accounts/{CLOUDFLARE_ACCOUNT_ID}/images/v1"
-    encoder = MultipartEncoder(
-        fields={"file": (image_file.name, image_file, "image/jpeg")}
-    )
-    headers = {
-        "Authorization": f"Bearer {CLOUDFLARE_API_TOKEN}",
-        "Content-Type": encoder.content_type,
-    }
-    response = requests.post(CLOUDFLARE_UPLOAD_URL, headers=headers, data=encoder)
-    print(f"[upload] {response} | {response.text}")
-
-    if response.status_code == 200:
-        return response.json()["result"]["variants"][0]
-    else:
-        st.error(f"Failed to upload: {response.text}")
-        return None
-
-
-if not CLOUDFLARE_ACCOUNT_ID:
-    st.error("Please input your Cloudflare Account ID on the sidebar")
-elif not CLOUDFLARE_API_TOKEN:
-    st.error("Please input your Cloudflare API Token on the sidebar")
-elif not OPENAI_API_KEY:
+if not OPENAI_API_KEY:
     st.error("Please input your OpenAI API Key on the sidebar")
 else:
     # Define OpenAI API Client
@@ -127,54 +78,42 @@ else:
 
             # Action to Cartoonize
             if st.button("Cartoonize your photo."):
-                # Upload Image on Cloudflare Storage
-                image_url = None
-                with st.spinner("Uploading..."):
-                    image_url = upload_image_to_cloudflare(uploaded_file)
+                # Transform Uploaded Image using OpenAI DALL·E API
+                cartoon_url = None
+                with st.spinner("Transforming..."):
+                    # Load ControlNet Model
+                    controlnet = ControlNetModel.from_pretrained(
+                        "lllyasviel/control_v11f1e_sd15_tile"
+                    )
 
-                if image_url:
-                    # Transform Uploaded Image using OpenAI DALL·E API
-                    cartoon_url = None
-                    with st.spinner("Transforming..."):
-                        # Load ControlNet Model
-                        controlnet = ControlNetModel.from_pretrained(
-                            "lllyasviel/control_v11f1e_sd15_tile"
-                        )
+                    # Setup Stable Diffusion Pipeline
+                    pipe = StableDiffusionControlNetPipeline.from_pretrained(
+                        "runwayml/stable-diffusion-v1-5",
+                        controlnet=controlnet,
+                        torch_dtype=torch.float16,
+                    ).to("cuda")
 
-                        # Setup Stable Diffusion Pipeline
-                        pipe = StableDiffusionControlNetPipeline.from_pretrained(
-                            "runwayml/stable-diffusion-v1-5",
-                            controlnet=controlnet,
-                            torch_dtype=torch.float16,
-                        ).to("cuda")
+                    # Run Transformation with Prompt
+                    art_style = selected_style.split(" | ")[1]
+                    prompt = (f"high quality, {art_style} cartoon style",)
+                    cartoon_url = pipe(prompt=prompt, image=image).images[0]
 
-                        # Download & Convert Image
-                        response = requests.get(image_url)
-                        image = Image.open(BytesIO(response.content))
+                if cartoon_url:
+                    st.success("✅ Transformed!")
 
-                        # Run Transformation with Prompt
-                        art_style = selected_style.split(" | ")[1]
-                        prompt = (f"high quality, {art_style} cartoon style",)
-                        cartoon_url = pipe(prompt=prompt, image=image).images[0]
+                    # Show Transformed Image
+                    st.image(
+                        cartoon_url,
+                        caption="Cartoonized Image",
+                        use_container_width=True,
+                    )
 
-                    if cartoon_url:
-                        st.success("✅ Transformed!")
+                    # Generate Summary on Image using LangChain
+                    description_prompt = f"Describe this cartoon-style image ({cartoon_url}) briefly in {OPENAI_LANGUAGE}.)"
+                    description = client.chat.completions.create(
+                        model=OPENAI_GPT_MODEL,
+                        messages=[{"role": "system", "content": description_prompt}],
+                    )
 
-                        # Show Transformed Image
-                        st.image(
-                            cartoon_url,
-                            caption="Cartoonized Image",
-                            use_container_width=True,
-                        )
-
-                        # Generate Summary on Image using LangChain
-                        description_prompt = f"Describe this cartoon-style image ({cartoon_url}) briefly in {OPENAI_LANGUAGE}.)"
-                        description = client.chat.completions.create(
-                            model=OPENAI_GPT_MODEL,
-                            messages=[
-                                {"role": "system", "content": description_prompt}
-                            ],
-                        )
-
-                        st.success("✅ Described!")
-                        st.write(description.choices[0].message.content)
+                    st.success("✅ Described!")
+                    st.write(description.choices[0].message.content)
--- app_replicate.py
+++ app_replicate.py
@@ -1,9 +1,11 @@
 from dotenv import dotenv_values
 from PIL import Image
+from requests_toolbelt.multipart.encoder import MultipartEncoder
 import base64
 import io
 import openai
 import replicate
+import requests
 import streamlit as st
 
 
@@ -19,13 +21,26 @@ with st.sidebar:
     IS_TEST = True
     config = dotenv_values(".env")
 
-    # LLM API Credential
+    # Generative AI API Credential
     REPLICATE_API_TOKEN = (
         st.text_input("Input your Replicate API Key", type="password")
         if IS_TEST == True
         else config["REPLICATE_API_TOKEN"]
     )
 
+    # Storage API Credential
+    CLOUDFLARE_ACCOUNT_ID = (
+        st.text_input("Input your Cloudflare Account ID", type="password")
+        if IS_TEST == True
+        else config["CLOUDFLARE_ACCOUNT_ID"]
+    )
+    CLOUDFLARE_API_TOKEN = (
+        st.text_input("Input your Cloudflare API Token", type="password")
+        if IS_TEST == True
+        else config["CLOUDFLARE_API_TOKEN"]
+    )
+
+    # LLM API Credential
     OPENAI_API_KEY = (
         st.text_input("Input your OpenAI API Key", type="password")
         if IS_TEST == True
@@ -55,8 +70,42 @@ with st.sidebar:
     st.write(f"[![Repo]({badge_link})]({github_link})")
 
 
+def upload_image_to_cloudflare(image_file):
+    CLOUDFLARE_VERIFY_URL = f"https://api.cloudflare.com/client/v4/accounts/{CLOUDFLARE_ACCOUNT_ID}/tokens/verify"
+    response = requests.get(
+        CLOUDFLARE_VERIFY_URL,
+        headers={"Authorization": f"Bearer {CLOUDFLARE_API_TOKEN}"},
+    )
+    print(f"[verify] {response} | {response.text}")
+
+    if response.status_code != 200:
+        st.error(f"Failed to verify: {response.text}")
+        return None
+
+    CLOUDFLARE_UPLOAD_URL = f"https://api.cloudflare.com/client/v4/accounts/{CLOUDFLARE_ACCOUNT_ID}/images/v1"
+    encoder = MultipartEncoder(
+        fields={"file": (image_file.name, image_file, "image/jpeg")}
+    )
+    headers = {
+        "Authorization": f"Bearer {CLOUDFLARE_API_TOKEN}",
+        "Content-Type": encoder.content_type,
+    }
+    response = requests.post(CLOUDFLARE_UPLOAD_URL, headers=headers, data=encoder)
+    print(f"[upload] {response} | {response.text}")
+
+    if response.status_code == 200:
+        return response.json()["result"]["variants"][0]
+    else:
+        st.error(f"Failed to upload: {response.text}")
+        return None
+
+
 if not REPLICATE_API_TOKEN:
     st.error("Please input your Replicate API Token on the sidebar")
+elif not CLOUDFLARE_ACCOUNT_ID:
+    st.error("Please input your Cloudflare Account ID on the sidebar")
+elif not CLOUDFLARE_API_TOKEN:
+    st.error("Please input your Cloudflare API Token on the sidebar")
 elif not OPENAI_API_KEY:
     st.error("Please input your OpenAI API Key on the sidebar")
 else:
@@ -129,9 +178,24 @@ else:
                         description = client.chat.completions.create(
                             model=OPENAI_GPT_MODEL,
                             messages=[
-                                {"role": "system", "content": description_prompt}
+                                {
+                                    "role": "system",
+                                    "content": description_prompt,
+                                }
                             ],
                         )
 
                         st.success("✅ Described!")
                         st.write(description.choices[0].message.content)
+
+                        # Upload Image on Cloudflare Storage
+                        response = requests.get(cartoon_url)
+                        if response.status_code == 200:
+                            image_data = response.content
+                            image_url = None
+                            with st.spinner("Uploading..."):
+                                image_url = upload_image_to_cloudflare(uploaded_file)
+                                print(image_url)
+
+                            if image_url:
+                                st.success("✅ Uploaded!")
