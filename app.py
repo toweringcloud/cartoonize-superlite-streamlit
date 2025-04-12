2f42125c6375ab2abcb60a57f952b69ce04cce0d
toweringcloud <toweringcloud@gmail.com>
2025-04-12 16:15:16 +0900

    finish

--- app.py
+++ app.py
@@ -22,12 +22,10 @@ if "CUSTOM_LOGIN_ID" in st.secrets:
     IMAGE_ACCOUNT_ID = st.secrets["CLOUDFLARE_ACCOUNT_ID"]
     IMAGE_API_URL = st.secrets["CLOUDFLARE_API_URL"]
     IMAGE_API_KEY = st.secrets["CLOUDFLARE_API_TOKEN_IMAGES"]
-    GPT_API_KEY = st.secrets["REPLICATE_API_TOKEN"]
-    GPT_MODEL1_C1 = st.secrets["REPLICATE_MODEL_DRAW1"]
-    GPT_MODEL1_C2 = st.secrets["REPLICATE_MODEL_DRAW2"]
-    GPT_MODEL1_C3 = st.secrets["REPLICATE_MODEL_DRAW3"]
-    GPT_API_KEY2 = st.secrets["OPENAI_API_KEY"]
-    GPT_MODEL2 = st.secrets["OPENAI_MODEL_DRAW"]
+    GPT_API_KEY1 = st.secrets["OPENAI_API_KEY"]
+    GPT_MODEL1 = st.secrets["OPENAI_MODEL_TTI"]
+    GPT_API_KEY2 = st.secrets["REPLICATE_API_TOKEN"]
+    GPT_MODEL2 = st.secrets["REPLICATE_MODEL_ITI"]
 else:
     config = dotenv_values(".env")
     LOGIN_ID = config["CUSTOM_LOGIN_ID"]
@@ -35,12 +33,10 @@ else:
     IMAGE_ACCOUNT_ID = config["CLOUDFLARE_ACCOUNT_ID"]
     IMAGE_API_URL = config["CLOUDFLARE_API_URL"]
     IMAGE_API_KEY = config["CLOUDFLARE_API_TOKEN_IMAGES"]
-    GPT_API_KEY = config["REPLICATE_API_TOKEN"]
-    GPT_MODEL1_C1 = config["REPLICATE_MODEL_DRAW1"]
-    GPT_MODEL1_C2 = config["REPLICATE_MODEL_DRAW2"]
-    GPT_MODEL1_C3 = config["REPLICATE_MODEL_DRAW3"]
-    GPT_API_KEY2 = config["OPENAI_API_KEY"]
-    GPT_MODEL2 = config["OPENAI_MODEL_DRAW"]
+    GPT_API_KEY1 = config["OPENAI_API_KEY"]
+    GPT_MODEL1 = config["OPENAI_MODEL_TTI"]
+    GPT_API_KEY2 = config["REPLICATE_API_TOKEN"]
+    GPT_MODEL2 = config["REPLICATE_MODEL_ITI"]
 
 
 def login():
@@ -102,12 +98,10 @@ with st.sidebar:
         "Choose a Cartoon Style",
         (
             "디즈니 | Pixar Disney",
-            "마블   | Marvel hero",
+            "마블 | Marvel Hero",
             "지브리 | Studio Ghibli",
-            "짱구   | Crayon shin-chan",
-            "김홍도 | Korean folk painting",
             "아이돌 | K-Pop Star",
-            "탤런트 | World-wide celebrity",
+            "미정 | User Prompt",
         ),
     )
 
@@ -137,19 +131,19 @@ with st.sidebar:
     # Transformation Strength (how many change - image)
     selected_change = st.slider(
         "Adjust a Transformation Strength",
-        min_value=0.0,
-        max_value=1.0,
+        min_value=0.1,
+        max_value=0.9,
         step=0.05,
-        value=0.5,  # default
+        value=0.75,  # default
     )
 
     # Guidance Scale (how to draw - prompt)
     selected_scale = st.slider(
         "Adjust a Guidance Scale",
-        min_value=0.0,
-        max_value=13.0,
-        step=0.5,
-        value=6.5,  # light (~4), natural (5~6), strong (9~12)
+        min_value=1,
+        max_value=15,
+        step=1,
+        value=10,  # natural (5~6), strong (9~12)
     )
 
     # Link to Github Repo
@@ -161,25 +155,35 @@ with st.sidebar:
 
 if not IMAGE_API_KEY:
     st.error("Please input your Cloudflare API Token on runtime configuration")
-elif not GPT_API_KEY:
-    st.error("Please input your Replicate API Token on runtime configuration")
+elif not GPT_API_KEY1:
+    st.error("Please input your OpenAI API Token on runtime configuration")
 elif not GPT_API_KEY2:
-    st.error("Please input your OpenAI API Key on runtime configuration")
+    st.error("Please input your LeonardoAI API Token on runtime configuration")
+elif not GPT_API_KEY3:
+    st.error("Please input your Replicate API Token on runtime configuration")
 else:
     # User Input Conditions
     input_condition = selected_input.split(" | ")[1]
-    art_style = selected_style.split(" | ")
+    drawing_style = selected_style.split(" | ")
+    drawing_style_name = (
+        "free" if drawing_style[1] == "User Prompt" else drawing_style[1]
+    )
+
+    # Define Assistant Prompt
+    assistant_prompt = ""
+    if drawing_style[1] == "Pixar Disney":
+        assistant_prompt = "A charming animated character in the style of classic Disney movies, with large expressive eyes, soft facial features, a whimsical and friendly smile, elegant proportions, smooth and clean line art, colorful and polished look, magical fairy-tale costume, warm and glowing lighting, set in a dreamy fantasy background, capturing the spirit of innocence and wonder,"
+    elif drawing_style[1] == "Marvel Hero":
+        assistant_prompt = "A powerful superhero character in the style of Marvel Comics, wearing a futuristic, high-tech suit with glowing elements, dynamic pose, detailed musculature, dramatic lighting, cinematic shadows, vibrant color palette, comic book realism, heroic expression, action-packed background, intense atmosphere, inspired by characters like Iron Man, Spider-Man, and Captain Marvel,"
+        selected_change = 0.5
+    elif drawing_style[1] == "Studio Ghibli":
+        assistant_prompt = "A dreamy, hand-painted fantasy scene inspired by Studio Ghibli, rendered in soft pastel tones. Lush, gently swaying grass fields, delicate wildflowers, and a small child with a curious gaze standing beneath a wide, cloud-filled sky. Warm, golden sunlight filtering through the clouds, creating a peaceful, nostalgic mood. Light watercolor textures, painterly brushstrokes, and subtle glowing dust particles in the air. Whimsical creatures or forest spirits watching from afar, evoking a sense of magic and wonder. Inspired by 'My Neighbor Totoro' and 'Spirited Away', with a gentle Japanese countryside atmosphere,"
+        selected_change = 0.65
+        selected_scale = 12
 
     if input_condition == "photo":
-        # Arrange Proper AI Model
-        GPT_MODEL = (
-            GPT_MODEL1_C3
-            if art_style == "Studio Ghibli"
-            else (GPT_MODEL1_C2 if art_style == "Crayon shin-chan" else GPT_MODEL1_C1)
-        )
-
         # Define Replicate API Client
-        replicate.client = replicate.Client(api_token=GPT_API_KEY)
+        replicate.client = replicate.Client(api_token=GPT_API_KEY2)
 
         # Accept User's Prompt
         uploaded_file = st.file_uploader(
@@ -195,9 +199,9 @@ else:
         )
 
         if uploaded_file is not None:
-            # Check File Size (Max 3MB)
-            if uploaded_file.size > 3 * 1024 * 1024:
-                st.warning("File size exceeds 3MB. Try again.")
+            # Check File Size (Max 5MB)
+            if uploaded_file.size > 5 * 1024 * 1024:
+                st.warning("File size exceeds 5MB. Try again.")
             else:
                 # Load Original Image
                 image = Image.open(uploaded_file)
@@ -216,26 +220,17 @@ else:
                     if image_url:
                         prompt_plus = f"""
                             A cartoon version of the input image, maintaining the same pose, background and facial expression. 
-                            Clean lines, bright colors, stylized like {art_style[1]} animation, but with the original subject's identity preserved. 
+                            Clean lines, bright colors, {drawing_style_name} style, but with the original subject's identity preserved. 
+                            {assistant_prompt if len(assistant_prompt) > 0 else ""}
                             {user_prompt if len(user_prompt) > 5 else ""}
                         """
-                        prompt_minus = """
-                            disfigured, kitsch, ugly, oversaturated, greain, 
-                            low-res, deformed, blurry, bad anatomy, poorly drawn face, 
-                            mutation, mutated, extra limb, poorly drawn hands, missing limb, 
-                            floating limbs, disconnected limbs, malformed hands, blur, out of focus, 
-                            long neck, long body, disgusting, poorly drawn, childish, 
-                            mutilated, mangled, old, surreal, calligraphy, 
-                            sign, writing, watermark, text, body out of frame, 
-                            extra legs, extra arms, extra feet, out of frame, poorly drawn feet, 
-                            cross-eye
-                        """
+                        prompt_minus = "disfigured, kitsch, ugly, oversaturated, greain, low-res, deformed, blurry, bad anatomy, poorly drawn face, mutation, mutated, extra limb, poorly drawn hands, missing limb, floating limbs, disconnected limbs, malformed hands, blur, out of focus, long neck, long body, disgusting, poorly drawn, childish, mutilated, mangled, old, surreal, calligraphy, sign, writing, watermark, text, body out of frame, extra legs, extra arms, extra feet, out of frame, poorly drawn feet, cross-eye"
 
                         # Transform custom image & prompt into cartoon using multi models
                         cartoon_url = None
                         with st.spinner("Transforming..."):
                             output = replicate.run(
-                                GPT_MODEL,
+                                GPT_MODEL2,
                                 input={
                                     "image": image_url,
                                     "prompt": prompt_plus,
@@ -259,13 +254,13 @@ else:
                         if cartoon_url:
                             st.image(
                                 cartoon_url,
-                                caption=f"{art_style[1]} style of cartoon",
+                                caption=f"{drawing_style_name} style of cartoon",
                                 use_container_width=True,
                             )
 
     else:
         # Define OpenAI API Client
-        client = openai.OpenAI(api_key=GPT_API_KEY2)
+        client = openai.OpenAI(api_key=GPT_API_KEY1)
 
         # Accept User's Prompt
         user_prompt = st.text_input("Enter your prompt (at least 10 characters):")
@@ -293,9 +288,13 @@ else:
                     cartoon_url = None
                     with st.spinner("Transforming..."):
                         response = client.images.generate(
-                            model=GPT_MODEL2,
+                            model=GPT_MODEL1,
                             size=selected_ratio.split(" | ")[1],
-                            prompt=f"A {art_style[1]} style of cartoonized image, {user_prompt}",
+                            prompt=f"""
+                                {drawing_style_name} style of cartoon, 
+                                {assistant_prompt if len(assistant_prompt) > 0 else ""}
+                                {user_prompt}
+                            """,
                             n=1,
                         )
                         cartoon_url = response.data[0].url
@@ -304,7 +303,7 @@ else:
                         # Show Transformed Image
                         st.image(
                             cartoon_url,
-                            caption=f"[{art_style[0]}] {user_prompt}",
+                            caption=f"[{drawing_style[0]}] {user_prompt}",
                             use_container_width=True,
                         )
             else:
